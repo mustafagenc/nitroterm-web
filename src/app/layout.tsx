9f6fd30eb793a190d686ca2ae1e792eaa036b20f
Mustafa Genc <eposta@mustafagenc.info>
2025-05-28 20:07:19 +0300

    Adds analytics to track user behavior
    Implements analytics to gather insights on user engagement.
    
    This allows for tracking user interactions and identifying areas for improvement.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Lexend, Fira_Code } from "next/font/google";
 import { Toaster } from "sonner";
 import { generateSiteMetadata } from "@lib/helpers";
+import { Analytics } from "@vercel/analytics/next";
 
 import "@radix-ui/themes/styles.css";
 import "../styles/globals.css";
@@ -47,6 +48,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
